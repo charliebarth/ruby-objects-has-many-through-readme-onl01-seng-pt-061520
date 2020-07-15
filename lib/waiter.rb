939e1fad293e243ef43e20120ff633fcccd1d5bf
Charlie Barth <charliepbarth@gmail.com>
2020-07-15 17:10:57 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -21,10 +21,12 @@ attr_accessor :name, :yrs_experience
   Meal.all.select do |meal|
     meal.waiter == self #checking for waiter now
   end
+end
 
 def best_tipper
   best_tipped_meal = meals.max do |meal_a, meal_b|
     meal_a.tip <=> meal_b.tip
   end
+end
  
 end
